4ea219606408e3cae7bc840193425273c30596d5
Joel Bidal <joelbidal1@hotmail.com>
2025-08-26 17:21:01 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,102 @@
+window.addEventListener('load', function() {   
+    // Date Picker initializer 
+    $(function(){
+        $("#datepicker").datepicker({
+            dateFormat: "dd/mm/yy",
+            duration: "fast"
+        });
+    }); 
+
+    // Fix navbar when scroll
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 1) {
+            $('nav').addClass('scrolled');
+        } 
+        else {
+            $('nav').removeClass('scrolled');
+        }
+    });
+
+    // Animations
+    let animatedElements = new Set();
+
+    let observer = new IntersectionObserver((entries) => {
+        const toAnimate = entries
+            .filter(entry => entry.isIntersecting && !animatedElements.has(entry.target))
+            .map(entry => entry.target);
+
+        if (toAnimate.length > 0) {
+            gsap.to(toAnimate, {
+                opacity: 1,
+                y: 0,
+                stagger: 0.15,
+                ease: "power2.out",
+                duration: 0.5,
+                delay: 0.3
+            });
+
+            toAnimate.forEach(el => {
+                animatedElements.add(el);
+                observer.unobserve(el);
+            });
+        }
+    }, {
+        threshold: 0.3
+    });
+
+    document.querySelectorAll("[data-fade]").forEach((el) => {
+        observer.observe(el);
+    });
+
+    // Slick slider for How It Works section
+    $('.slide-how-it-works').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding:'20px',
+        focusOnSelect: false,
+        arrows: false,
+        autoplay: false,
+        pauseOnHover: false,
+        draggable: true,
+        infinite: false,
+        dots: true,
+    });
+
+    if ($('#waitlist-form').length) {
+        $(function () {
+            const scriptURL =
+                'https://script.google.com/macros/s/AKfycbyQKeIek4qX7uYjlhtLJ9nvotj5Ab8vJbLPpsDYaf6NYD3KeQN3xBybpZiCwtExJ5gS/exec';
+            const form = document.getElementById('waitlist-form');
+        
+            form.addEventListener('submit', (e) => {
+                $('#waitlist-form').addClass('disabled');
+        
+                // Sending status
+                $('#waitlist-form').addClass('readonly');
+                $('#waitlist-form input.button').val("Sending Information");
+        
+                e.preventDefault();
+        
+                // Crear FormData para incluir archivo y otros campos
+                const formData = new FormData(form);
+        
+                fetch(scriptURL, { method: 'POST', body: formData })
+                .then((response) => {
+                    $('.success-form').addClass('visible');
+                    $('#waitlist-form').addClass('readonly');
+                    $('.wrapper-reserve-area').addClass('readonly');
+                    $('#waitlist-form input.button').val("Submitted");
+                })
+                .catch((error) => {
+                    console.error('Error!', error.message);
+                    $('.success-form').removeClass('visible');
+                    $('#waitlist-form').removeClass('readonly');
+                    $('.wrapper-reserve-area').removeClass('readonly');
+                    $('#waitlist-form input.button').val("Submit");
+                });
+            });
+        });
+    }
+});
